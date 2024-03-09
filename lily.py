25258bf57766908118d4eeaa1b75a661344b2cfa
Yuhang Song <1595025647@qq.com>
2024-03-09 18:39:16 +0800

    testing

--- lily.py
+++ lily.py
@@ -84,7 +84,7 @@ class Lily(PreTrainedModel):
             output_all_encoded_layers=False,
         )
 
-
+        print(pooled_output_t.shape)
 
         linguistic_prediction, vision_prediction, _ = self.cls(
             sequence_output_t, sequence_output_v, pooled_output_t, pooled_output_v
