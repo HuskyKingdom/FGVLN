d1d2984e1f8b6eea9473e45856838ba77e2bd412
Yuhang Song <1595025647@qq.com>
2024-03-10 13:06:56 +0800

    testing

--- utils/dataset/all_dataset.py
+++ utils/dataset/all_dataset.py
@@ -190,10 +190,12 @@ class BaseDataset(Dataset):
         f, b, p, m = self._get_visual_features(positive_ids)
         features, boxes, probs, masks = [f], [b], [p], [m] # This feature will patch to the longest length (8)
 
-        print(probs[0].shape)
+        
         
         if self._traj_judge: # Trajectory judgment task
             negative_traj = negative_captions + negative_images + negative_random
+
+            print("n: {} \n \n".format(negative_traj))
             for traj in negative_traj:
                 instructions += [instructions[0]]
                 f, b, p, m = self._get_visual_features(traj)
