a8bdfc0d7c5bfa6001014fa2c2f737a11e40dc58
Yuhang Song <1595025647@qq.com>
2024-03-12 18:09:11 +0800

    testing

--- vis_exp.py
+++ vis_exp.py
@@ -235,8 +235,8 @@ class VisDataset(YTbDataset):
 
         new_list_1 = positive_ids[:] 
         
+        new_list_1[1] = negative_random[1][-2]
         new_list_1[2] = negative_random[1][-1]
-        # new_list_1[2] = negative_images[1][1]
         # new_list_1[3] = negative_random[1][2]
         
 
