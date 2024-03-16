5ddda41ffe75c1157c512b9d82b37c3eb7fe29ab
Yuhang Song <1595025647@qq.com>
2024-03-16 16:06:25 +0800

    test

--- lily.py
+++ lily.py
@@ -68,12 +68,9 @@ class Lily(PreTrainedModel):
         order_atteneded_visual_feature=None,
     ) -> Dict[str, torch.Tensor]:
         
-        print(image_features.shape) # [14, 808, 2048]
-        print(instr_tokens.shape) # [14, 60]
-        print(attention_mask.shape)
-        print(image_attention_mask.shape)
-        print(co_attention_mask.shape)
-
+        print("image_features {} | instr_tokens {} | attention_mask {} | image_attention_mask {} | co_attention_mask {} \n\n".
+              format(image_features.shape,instr_tokens.shape,attention_mask.shape,image_attention_mask.shape,co_attention_mask.shape))
+  
         (
             sequence_output_t,
             sequence_output_v,
