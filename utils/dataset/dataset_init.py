d3b512a46b271fc23303c2dc15b2c8bd7e8d8504
Yuhang Song <1595025647@qq.com>
2024-03-16 15:44:12 +0800

    test

--- utils/dataset/dataset_init.py
+++ utils/dataset/dataset_init.py
@@ -160,7 +160,7 @@ def load_dataloader(args, default_gpu, logger, local_rank) -> str:
     logger.info("Loading val datasets")
     
     if not args.no_test and not args.pretrain:
-        val_seen_dataset = load_BeamDataset("val_seen", args, tokenizer, features_reader, default_gpu, Train=False)
+        val_seen_dataset = load_BeamDataset("val_seen", args, tokenizer, features_reader, default_gpu, Train=True)
         val_unseen_dataset = load_BeamDataset("val_unseen", args, tokenizer, features_reader, default_gpu, Train=True) # CHANGE~
     
         if args.pretrain:
