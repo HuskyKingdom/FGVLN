1caabf888062fe3e3985934ac91db200d07c47b6
Yuhang Song <1595025647@qq.com>
2024-05-03 14:58:40 +0800

    ignore paddings

--- utils/dataset/all_dataset.py
+++ utils/dataset/all_dataset.py
@@ -889,6 +889,9 @@ class BeamDataset(Dataset):
 
                 for timestep in range(len(M[mask])):
                     print(f"M len {len(M[mask])} Pos path len {len(positive_path_feature[elem])} FGN {len(FGN)} ")
+                    x = FGN[timestep][timestep]
+                    x =  M[mask][timestep]
+                    x = positive_path_feature[elem][timestep]
                     FGN[timestep] = replace_feature[elem] if M[mask][timestep] == 1 else positive_path_feature[elem][timestep]
                 
 
