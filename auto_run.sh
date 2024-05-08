4e58f8f49a6889dddc59ae1b4b6269755222bd9e
Hang <me@yhscode.com>
2024-05-08 21:25:36 +0800

    Add files via upload

--- auto_run.sh
+++ auto_run.sh
@@ -3,25 +3,36 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch  \
     --master_port 5558   \
     -m train   \
     --from_pretrained result/FGvln_1phase/data/29.bin \
-    --save_name FGvln_3e5_1FG_5it_mini  \
+    --save_name FGvln_baseline_1  \
     --shuffle_visual_features   \
     --ranking   \
     --batch_size 8    \
     --num_epochs 10 \
-    --FGN \
-    --trial_type 1 \
-    --num_FGN 1 \
-    --trial_iter 5 \
     --mini
 
-echo "FGvln_3e5_1FG_5it_mini Done..."
+echo "Baseline 1 Done..."
+
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch  \
     --nproc_per_node 4   \
     --master_port 5558   \
     -m train   \
     --from_pretrained result/FGvln_1phase/data/29.bin \
-    --save_name FGvln_3e5_2FG_5it   \
+    --save_name FGvln_baseline_2  \
+    --shuffle_visual_features   \
+    --ranking   \
+    --batch_size 8    \
+    --num_epochs 10 \
+    --mini
+
+echo "Baseline 2 Done..."
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch  \
+    --nproc_per_node 4   \
+    --master_port 5558   \
+    -m train   \
+    --from_pretrained result/FGvln_1phase/data/29.bin \
+    --save_name FGvln_3e5_2FG_5it_1   \
     --shuffle_visual_features   \
     --ranking   \
     --batch_size 8    \
@@ -30,47 +41,30 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch  \
     --trial_type 1 \
     --num_FGN 2 \
     --trial_iter 5 \
-    --mini
-
-echo "FGvln_3e5_2FG_5it_mini Done..."
-
-
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch  \
-    --nproc_per_node 4   \
-    --master_port 5558   \
-    -m train   \
-    --from_pretrained result/FGvln_1phase/data/29.bin \
-    --save_name FGvln_3e5_2FG_3it_mini  \
-    --shuffle_visual_features   \
-    --ranking   \
-    --batch_size 8    \
-    --num_epochs 10 \
-    --FGN \
-    --trial_type 1 \
-    --num_FGN 2 \
-    --mini
-
-
-echo "FGvln_3e5_2FG_3it_mini Done..."
-
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch  \
-    --nproc_per_node 4   \
-    --master_port 5558   \
-    -m train   \
-    --from_pretrained result/FGvln_1phase/data/29.bin \
-    --save_name FGvln_3e5_2FG_3it_out-domain_mini  \
-    --shuffle_visual_features   \
-    --ranking   \
-    --batch_size 8    \
-    --num_epochs 10 \
-    --FGN \
-    --trial_type 1 \
-    --num_FGN 2 \
     --FG_style 1 \
     --mini
 
+echo "FGvln_3e5_2FG_5it_mini 1 Done..."
 
-echo "FGvln_3e5_2FG_3it_out-domain_mini Done..."
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch  \
+    --nproc_per_node 4   \
+    --master_port 5558   \
+    -m train   \
+    --from_pretrained result/FGvln_1phase/data/29.bin \
+    --save_name FGvln_3e5_2FG_5it_2   \
+    --shuffle_visual_features   \
+    --ranking   \
+    --batch_size 8    \
+    --num_epochs 10 \
+    --FGN \
+    --trial_type 1 \
+    --num_FGN 2 \
+    --trial_iter 5 \
+    --FG_style 1 \
+    --mini
+
+echo "FGvln_3e5_2FG_5it_mini 2 Done..."
+
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch  \
     --nproc_per_node 4   \
